506fad94ccc1d2aa6751483e466906f8bd230d36
Mark John <markjohnbatasinin707@gmail.com>
2024-11-06 16:03:33 -0800

    awd

--- assets/js/johndoe.js
+++ assets/js/johndoe.js
@@ -60,7 +60,7 @@ $(window).on("load", function() {
 function initMap() {
 // Styles a map in night mode.
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.674, lng: -73.945},
+      center: {lat: 12.8797, lng: 121.7740},
         zoom: 12,
         scrollwheel:  false,
         navigationControl: false,
